dc23ebe88b29cc71f0a331b022d70f5a5dc02be8
Ceren Demirci <cerdemrc@gmail.com>
2021-07-04 15:43:24 +0300

    add projects to json

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -3,205 +3,30 @@
     <div class="projects">
       <b-container>
         <b-row>
-          <b-col xs="12" sm="12" md="6" lg="4">
+          <b-col
+            xs="12"
+            sm="12"
+            md="6"
+            lg="4"
+            v-for="project in Projects"
+            :key="project.name"
+          >
             <div class="projects-card">
               <img
                 class="projects-card-image"
-                src="../assets/projects/makeup.png"
+                :src="require('../assets/projects/' + project.image)"
               />
-              <h3 class="projects-card-title">Find Make-Up</h3>
+              <h3 class="projects-card-title">{{ project.name }}</h3>
               <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/makeup"
-                  target="_blank"
-                  title="See Code"
+                <a :href="project.code" target="_blank" title="See Code"
                   ><img src="../assets/svg/see-code.svg"
                 /></a>
-                <a
-                  href="https://cerdemrc.github.io/makeup/"
-                  target="_blank"
-                  title="See Project"
+                <a :href="project.project" target="_blank" title="See Project"
                   ><img src="../assets/svg/go-project.svg"
                 /></a>
               </div>
             </div>
           </b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/random-things.png"
-              />
-              <h3 class="projects-card-title">Random Things</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/random-things-react"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://random-things-react.vercel.app/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/pixabay.png"
-              />
-              <h3 class="projects-card-title">Pixabay API</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/pixabay-api"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://cerdemrc.github.io/pixabay-api/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/ideal-weight.png"
-              />
-              <h3 class="projects-card-title">Ideal Weight Calculator</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/ideal-weight-calculator"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://cerdemrc.github.io/ideal-weight-calculator/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/quiz-app.png"
-              />
-              <h3 class="projects-card-title">Quiz App</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/quiz-app"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://cerdemrc.github.io/quiz-app/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/currency.png"
-              />
-              <h3 class="projects-card-title">Currency Converter</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/currency-converter"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://cerdemrc.github.io/currency-converter/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/rps.png"
-              />
-              <h3 class="projects-card-title">Rock Paper Scissors</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/vue-rock-paper-scissors"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://vuejs-rock-paper-scissors.netlify.app/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/movie.png"
-              />
-              <h3 class="projects-card-title">Movie App</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/movie-app"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://application-movie.netlify.app/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
-          <b-col xs="12" sm="12" md="6" lg="4">
-            <div class="projects-card">
-              <img
-                class="projects-card-image"
-                src="../assets/projects/instagram.png"
-              />
-              <h3 class="projects-card-title">Instagram Clone</h3>
-              <div class="projects-card-links">
-                <a
-                  href="https://github.com/cerdemrc/instagram-clone"
-                  target="_blank"
-                  title="See Code"
-                  ><img src="../assets/svg/see-code.svg"
-                /></a>
-                <a
-                  href="https://instagram-clone-git-master-cerdemrc.vercel.app/"
-                  target="_blank"
-                  title="See Project"
-                  ><img src="../assets/svg/go-project.svg"
-                /></a>
-              </div></div
-          ></b-col>
         </b-row>
       </b-container>
     </div>
@@ -209,8 +34,14 @@
 </template>
 
 <script>
+import Projects from "../data/projects.json";
 export default {
   name: "Portfolio",
+  data() {
+    return {
+      Projects,
+    };
+  },
 };
 </script>
 
